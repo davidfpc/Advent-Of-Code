90b0cadb2944bc657fefc951b1b5a0dbb99596d4
David Cardoso <davidcardopinto@hotmail.com>
2023-12-01 20:33:02 +0000

    2023 setup and solution to Day 1: Trebuchet?!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Advent-Of-Code
-Repository to store the challenges for Advent Of Code
+
+This repository contains programs to solve Advent of Code puzzles.
+
+Completion: (⭐ first part, 🌟 both parts):
+
+## [2023](aoc2023):
+
+**Language:** Kotlin
+
+| Day        | 01 | 02 | 03 | 04 | 05 | 06 | 07 | 08 | 09 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 |
+|------------|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|----|
+| Completion | 🌟 |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |
---
+++ aoc2023/day1.kt
@@ -0,0 +1,47 @@
+package aoc2023
+
+import java.io.File
+
+fun main() {
+    Day1.execute()
+}
+
+private object Day1 {
+
+    fun execute() {
+        val input = readInput()
+        println("Part 1: ${part1(input)}")
+        println("Part 2: ${part2(input)}")
+    }
+
+    private fun part1(input: List<String>): Int = input.sumOf {
+        val values = it.filter { value -> value.isDigit() }
+        if (values.isEmpty()) {
+            println("No numbers found! <<$it>>")
+            0
+        } else {
+            "${values.first()}${values.last()}".toInt()
+        }
+    }
+
+    private val REPLACEMENTS: List<Pair<String, String>> = listOf(
+        "one" to "o1e",
+        "two" to "t2o",
+        "three" to "t3e",
+        "four" to "4",
+        "five" to "5e",
+        "six" to "6",
+        "seven" to "7n",
+        "eight" to "e8t",
+        "nine" to "n9e"
+    )
+
+    private fun part2(input: List<String>): Int {
+        val mappedInput = input.map {
+            REPLACEMENTS.fold(it) { value, rep -> value.replace(rep.first, rep.second) }
+        }
+        return part1(mappedInput)
+    }
+
+    private fun readInput(): List<String> = File("aoc2023/inputFiles/day1.txt").readLines()
+}
