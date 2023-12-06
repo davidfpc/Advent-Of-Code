26398a1273700242aeab916027fe41544137654a
David Cardoso <davidcardopinto@hotmail.com>
2023-12-06 12:10:26 +0000

    Day 6: Wait For It

---
+++ aoc2023/day6.kt
@@ -0,0 +1,33 @@
+package aoc2023
+
+import utils.InputRetrieval
+
+fun main() {
+    Day6.execute()
+}
+
+private object Day6 {
+    fun execute() {
+        val input = readInput()
+        println("Part 1: ${part1(input)}")
+        println("Part 2: ${part2(input)}")
+    }
+
+    private fun part1(input: List<Race>): Long = input
+        .map { race -> (0L..race.time).filter { holdingTime: Long -> holdingTime * (race.time - holdingTime) > race.recordDistance }.size.toLong() }
+        .reduce { acc: Long, i: Long -> acc * i }
+
+    private fun part2(input: List<Race>): Long {
+        val race = input.reduce { acc, race -> Race("${acc.time}${race.time}".toLong(), "${acc.recordDistance}${race.recordDistance}".toLong()) }
+        return part1(listOf(race))
+    }
+
+    private fun readInput(): List<Race> {
+        val (rawTime, rawDistance) = InputRetrieval.getFile(2023, 6).readLines()
+        val time = rawTime.removePrefix("Time:").split(' ').filter { it.isNotBlank() }
+        val distance = rawDistance.removePrefix("Distance:").split(' ').filter { it.isNotBlank() }
+        return time.mapIndexed { i, c -> Race(c.toLong(), distance[i].toLong()) }
+    }
+
+    data class Race(val time: Long, val recordDistance: Long)
+}
